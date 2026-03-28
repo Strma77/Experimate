8caa2547093d780f04876b5fd7f39d1e7ffa00e0
David Topić <codedavidtopic@gmail.com>
2026-03-28 12:59:58 +0100

    Add SecurityConfig beans: AuthenticationProvider and   AuthenticationManager

--- src/main/java/hr/tvz/experimate/experimate/config/SecurityConfig.java
+++ src/main/java/hr/tvz/experimate/experimate/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package hr.tvz.experimate.experimate.config;
 
+import hr.tvz.experimate.experimate.security.AppUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration      //For Spring to know this is the configuration class
@@ -12,10 +17,29 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    //TODO napravi da ovo zapravo filtrira http requestove umijesto da samo propusta sve
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http.csrf(CsrfConfigurer::disable)
-                .authorizeHttpRequests(request -> request.anyRequest().permitAll());
+                .authorizeHttpRequests(request ->
+                        request.anyRequest().permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(AppUserDetailsService userDetailsService) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(DaoAuthenticationProvider authenticationProvider) {
+        return new ProviderManager(authenticationProvider);
     }
 }

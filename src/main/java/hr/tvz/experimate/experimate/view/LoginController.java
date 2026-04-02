555fe785c0bd627fe43a605097c0cce7870154a3
David Topić <codedavidtopic@gmail.com>
2026-04-02 16:37:02 +0200

    Add view/LoginController

---
+++ src/main/java/hr/tvz/experimate/experimate/view/LoginController.java
@@ -0,0 +1,16 @@
+package hr.tvz.experimate.experimate.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("currentPage", "login");
+        return  "login";
+    }
+
+}

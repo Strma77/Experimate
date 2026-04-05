1edf28626faae0c0fe97a85746121c82c667c516
David Topić <codedavidtopic@gmail.com>
2026-04-05 11:35:58 +0200

    Fix booking_request package for frontend.

--- src/main/java/hr/tvz/experimate/experimate/model/booking_request/BookingRequestResponse.java
+++ src/main/java/hr/tvz/experimate/experimate/model/booking_request/BookingRequestResponse.java
@@ -1,5 +1,13 @@
 package hr.tvz.experimate.experimate.model.booking_request;
 
+import hr.tvz.experimate.experimate.model.shared.TourListingDetails;
+import hr.tvz.experimate.experimate.model.shared.UserDetails;
+
+import java.time.LocalDateTime;
+
 public record BookingRequestResponse(Integer id,
-                                     BookingRequestStatus status) {
+                                     BookingRequestStatus status,
+                                     LocalDateTime requestDate,
+                                     TourListingDetails tourListing,
+                                     UserDetails user) {
 }

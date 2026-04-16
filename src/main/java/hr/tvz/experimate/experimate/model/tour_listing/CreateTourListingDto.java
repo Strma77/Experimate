ad2d56edddb28854daf605b3e238a4e65a668e39
David Topić <codedavidtopic@gmail.com>
2026-04-16 15:12:34 +0200

    Add not null tag to meetingDate, longitude and latitude in CreateTourListingDTO

--- src/main/java/hr/tvz/experimate/experimate/model/tour_listing/CreateTourListingDto.java
+++ src/main/java/hr/tvz/experimate/experimate/model/tour_listing/CreateTourListingDto.java
@@ -1,7 +1,6 @@
 package hr.tvz.experimate.experimate.model.tour_listing;
 
 import hr.tvz.experimate.experimate.model.shared.Constraints;
-import hr.tvz.experimate.experimate.model.user.User;
 import jakarta.validation.constraints.*;
 
 import java.time.LocalDateTime;
@@ -9,9 +8,9 @@ import java.time.LocalDateTime;
 public record CreateTourListingDto(
         @Positive Integer hostId,
         @NotBlank String city,
-        @DecimalMin("-180.0") @DecimalMax("180.0") Double longitude,
-        @DecimalMin("-90.0") @DecimalMax("90.0") Double latitude,
-        @Future LocalDateTime meetingDate,
+        @NotNull @DecimalMin("-180.0") @DecimalMax("180.0") Double longitude,
+        @NotNull @DecimalMin("-90.0") @DecimalMax("90.0") Double latitude,
+        @NotNull @Future LocalDateTime meetingDate,
         @NotBlank @Size(
                 min = Constraints.TourListingConstraints.TOUR_DESCRIPTION_MIN,
                 max = Constraints.TourListingConstraints.TOUR_DESCRIPTION_MAX)
